337051b0f6bf751e8d337c080e8e3c5350810a7d
anandmurti012 <anand.raj431@gmail.com>
2023-09-07 13:46:33 +0530

    changes

---
+++ src/component/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import HighlightIcon from '@mui/icons-material/Highlight';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <h1><ListAltIcon />TaskFolio</h1>
+      </header>
+    </>
+  );
+};
+
+export default Header;
